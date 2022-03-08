4ded8d9d50abf0435eeee5cd5d9c6f7496db7356
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-03-07 18:27:54 -0800

    Time: 63 ms (73.38%), Space: 17.6 MB (74.54%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        
+        if not head:
+            return None
+        
+        if not head.next:
+            return None
+        
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return True
+        
+        return False
