8bba9e82d03489e84aebdb7a091cd653d8b9c513
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-03-14 23:17:52 -0700

    Time: 63 ms (6.99%), Space: 13.9 MB (72.19%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,19 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        if n < 3:
+            return n
+        
+        # initialize dp array
+        dp = [0]* (n+1)
+        dp[1] = 1
+        dp[2] = 2
+        
+        i = 3
+        while i <= n:
+            
+            dp[i] = dp[i - 1] + dp[i - 2]
+            i += 1
+        
+        return dp[n]
+            
