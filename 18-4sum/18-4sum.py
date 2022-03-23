05cb76333771bc440bcd921c5deb1ce6164d89f6
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-03-22 22:07:15 -0700

    Time: 788 ms (67.78%), Space: 13.6 MB (30.44%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -39,41 +39,6 @@ class Solution(object):
                 left += 1  # we need a pair with a bigger sum
             else:
                 right -= 1  # we need a pair with a smaller sum
-#         nums.sort()
-#         quadruplets = []
-#         for i in range(len(nums) - 3):
-#             if i > 0 and nums[i] == nums[i-1]:
-#                 continue
-#             for j in range(i + 1, len(nums) - 2):
-#                 if j > i + 1 and nums[j] == nums[j - 1]:
-#                     continue
-#                 self.search(i, j, quadruplets, nums, target)
-        
-#         return quadruplets
-    
-#     def search(self, i, j, quadruplets, nums, target):
-        
-#         left = j + 1
-#         right = len(nums) - 1
-        
-#         while left < right:
-            
-#             current_sum = nums[i] + nums[j] + nums[left] + nums[right]
-            
-#             if current_sum == target:
-#                 quadruplets.append([nums[i], nums[j], nums[left], nums[right]])
-#                 left += 1
-#                 right -= 1
                 
-#                 while (left < right and nums[left] == nums[left - 1]):
-#                     left += 1
-                
-#                 while (left < right and nums[right] == nums[right - 1]):
-#                     right -= 1
-            
-#             elif current_sum < target:
-#                 left += 1
-            
-#             else:
-#                 right -= 1
+
         
