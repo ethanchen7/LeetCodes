a4e7d787cca88c981ed2cd9da65761e5f98210ce
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-03-22 22:05:06 -0700

    Time: 1208 ms (40.24%), Space: 13.1 MB (98.23%) - LeetHub

---
+++ 18-4sum/18-4sum.py
@@ -0,0 +1,79 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        quadruplets = []
+        for i in range(0, len(nums)-3):
+        # skip same element to avoid duplicate quadruplets
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums)-2):
+            # skip same element to avoid duplicate quadruplets
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                self.search_pairs(nums, target, i, j, quadruplets)
+        return quadruplets
+
+
+    def search_pairs(self, nums, target_sum, first, second, quadruplets):
+        left = second + 1
+        right = len(nums) - 1
+        while (left < right):
+            quad_sum = nums[first] + nums[second] + nums[left] + nums[right]
+            
+            if quad_sum == target_sum:  # found the quadruplet
+                quadruplets.append(
+                [nums[first], nums[second], nums[left], nums[right]])
+                left += 1
+                right -= 1
+                while (left < right and nums[left] == nums[left - 1]):
+                    left += 1  # skip same element to avoid duplicate quadruplets
+                while (left < right and nums[right] == nums[right + 1]):
+                    right -= 1  # skip same element to avoid duplicate quadruplets
+                    
+            elif quad_sum < target_sum:
+                left += 1  # we need a pair with a bigger sum
+            else:
+                right -= 1  # we need a pair with a smaller sum
+#         nums.sort()
+#         quadruplets = []
+#         for i in range(len(nums) - 3):
+#             if i > 0 and nums[i] == nums[i-1]:
+#                 continue
+#             for j in range(i + 1, len(nums) - 2):
+#                 if j > i + 1 and nums[j] == nums[j - 1]:
+#                     continue
+#                 self.search(i, j, quadruplets, nums, target)
+        
+#         return quadruplets
+    
+#     def search(self, i, j, quadruplets, nums, target):
+        
+#         left = j + 1
+#         right = len(nums) - 1
+        
+#         while left < right:
+            
+#             current_sum = nums[i] + nums[j] + nums[left] + nums[right]
+            
+#             if current_sum == target:
+#                 quadruplets.append([nums[i], nums[j], nums[left], nums[right]])
+#                 left += 1
+#                 right -= 1
+                
+#                 while (left < right and nums[left] == nums[left - 1]):
+#                     left += 1
+                
+#                 while (left < right and nums[right] == nums[right - 1]):
+#                     right -= 1
+            
+#             elif current_sum < target:
+#                 left += 1
+            
+#             else:
+#                 right -= 1
+        
