913cfd5afe38a10f2b5fa03fa0e97dc6030effa5
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-03-25 10:50:17 -0700

    Time: 152 ms (41.71%), Space: 13.9 MB (50.53%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,21 +1,29 @@
-class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        
-        start, max_len, max_repeating_chars = 0, 0, 0
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
         char_frequency = {}
+        max_len, max_repeating_chars = 0, 0
+        start = 0
         
         for end in range(len(s)):
+            
             right = s[end]
-            if right not in char_frequency:
+            if right not in char_frequency: 
                 char_frequency[right] = 0
             char_frequency[right] += 1
             max_repeating_chars = max(max_repeating_chars, char_frequency[right])
-            
-            if (end - start + 1 - max_repeating_chars) > k:
+
+            if ((end - start + 1 - max_repeating_chars) > k):
+                print(start, end)
                 left = s[start]
                 char_frequency[left] -= 1
                 start += 1
             
             max_len = max(max_len, end - start + 1)
-        
+            
         return max_len
+            
