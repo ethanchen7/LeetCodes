a30a458c6747d46aacbe764391ef23a0a9410db5
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-03-24 23:19:17 -0700

    Time: 832 ms (53.19%), Space: 17.9 MB (91.25%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,22 +1,27 @@
-class Solution:
-    def totalFruit(self, fruits: List[int]) -> int:
-        
-        type_frequency = {}
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        char_frequency = {}
         start = 0
-        maxFruits = 0
+        max_length = 0
         
-        for end in range(0, len(fruits)):
-            if fruits[end] not in type_frequency:
-                type_frequency[fruits[end]] = 0
-            type_frequency[fruits[end]] += 1
-        
-            while len(type_frequency) > 2:
+        for end in range(len(fruits)):
+            if fruits[end] not in char_frequency:
+                char_frequency[fruits[end]] = 0
+            char_frequency[fruits[end]] += 1
+            
+            while len(char_frequency) > 2:
                 left = fruits[start]
-                type_frequency[left] -= 1
-                if type_frequency[left] == 0:
-                    del type_frequency[left]
+                char_frequency[left] -= 1
+                if char_frequency[left] == 0:
+                    del char_frequency[left]
                 start += 1
             
-            maxFruits = max(maxFruits, end - start + 1)
+            max_length = max(max_length, end - start + 1)
         
-        return maxFruits
+        return max_length
+             
+            
