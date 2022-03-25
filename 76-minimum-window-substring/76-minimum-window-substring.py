cf0358d40485270fa65fcc7a9f2c3f008e6a0a2e
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-03-25 14:04:49 -0700

    Time: 87 ms (87.84%), Space: 16.5 MB (28.73%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,9 +1,17 @@
-class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        
-        start, matched = 0, 0
-        min_length = len(s) + 1
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        start = 0
         char_frequency = {}
+        matched = 0
+        min_len = len(s) + 1
+        
+        if s == t:
+            return s
         
         for char in t:
             if char not in char_frequency:
@@ -11,27 +19,27 @@ class Solution:
             char_frequency[char] += 1
         
         for end in range(len(s)):
-            right_char = s[end]
-            if right_char in char_frequency:
-                char_frequency[right_char] -= 1
-                if char_frequency[right_char] >= 0:
+            right = s[end]
+            
+            if right in char_frequency:
+                char_frequency[right] -= 1
+                if char_frequency[right] >= 0:
                     matched += 1
             
             while matched == len(t):
-                
-                if end - start + 1 < min_length:
-                    min_length = end - start + 1
+                substring_length = end - start + 1
+                if substring_length < min_len:
+                    min_len = substring_length
                     substr_start = start
-                
-                left_char = s[start]
+                    
+                left = s[start]
                 start += 1
-                
-                if left_char in char_frequency:
-                    if char_frequency[left_char] == 0:
+                if left in char_frequency:
+                    if char_frequency[left] == 0:
                         matched -= 1
-                    char_frequency[left_char] += 1
+                    char_frequency[left] += 1
         
-        if min_length == len(s) + 1:
-            return ''
+        if min_len == len(s) + 1:
+            return ""
         
-        return s[substr_start:substr_start + min_length]
+        return s[substr_start:substr_start + min_len]
