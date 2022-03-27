9fbd7ed1c707fe1c5e7ae16291ad2d3f1b94b7a3
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-03-26 17:52:22 -0700

    Time: 46 ms (56.51%), Space: 14 MB (20.92%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,26 +1,21 @@
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
+        
         low, i = 0, 0
         high = len(nums) - 1
         
         while i <= high:
-            
-            # move all the 0s to the low pointer
-            # move low and i forward, because we know everything at the low is sorted
             if nums[i] == 0:
                 nums[low], nums[i] = nums[i], nums[low]
                 i += 1
                 low += 1
-            
+                
             elif nums[i] == 1:
                 i += 1
-            
-            # move all the 2s to the high pointer
-            # decrement high but not i, since we still need to check the new i
+                
             else:
                 nums[i], nums[high] = nums[high], nums[i]
                 high -= 1
