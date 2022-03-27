15691a8936c232efa35c4581cfefdf9adfbba8aa
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-03-27 12:53:28 -0700

    Time: 42 ms (59.00%), Space: 13.8 MB (98.43%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,23 +1,19 @@
-class Solution(object):
-    def backspaceCompare(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        
         index1 = len(s) - 1
         index2 = len(t) - 1
         
-        while (index1 >= 0 or index2 >= 0):
+        while index1 >= 0 or index2 >= 0:
             
-            i1 = self.get_valid_char_idx(s, index1)
-            i2 = self.get_valid_char_idx(t, index2)
+            i1 = self.find_next_valid_char(s, index1)
+            i2 = self.find_next_valid_char(t, index2)
             
             if i1 < 0 and i2 < 0:
-                return True # if both reach the end of the strings
+                return True
             
             if i1 < 0 or i2 < 0:
-                return False # if only one of the strings reach the end
+                return False
             
             if s[i1] != t[i2]:
                 return False
@@ -25,21 +21,21 @@ class Solution(object):
             index1 = i1 - 1
             index2 = i2 - 1
         
-        return True # if both hit zero
-            
+        return True
     
-    def get_valid_char_idx(self, stri, index):
+    def find_next_valid_char(self, string, index):
         
         backspaces = 0
         
         while index >= 0:
-            if stri[index] == "#":
+            if string[index] == "#":
                 backspaces += 1
             elif backspaces > 0:
                 backspaces -= 1
             else:
                 break
-        
+            
             index -= 1
         
         return index
+            
