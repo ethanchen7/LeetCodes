77cc68731b0ab9e7fb6d2e98bf5088474bc904cd
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-03-27 16:40:11 -0700

    Time: 45 ms (62.72%), Space: 13.8 MB (65.82%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        slow = n
+        fast = n
+        
+        while True:
+            slow = self.find_square_sum(slow)
+            fast = self.find_square_sum(self.find_square_sum(fast))
+            
+            if slow == fast:
+                break
+        
+        return slow == 1
+    
+    
+    def find_square_sum(self, num):
+        _sum = 0
+        
+        while num > 0:
+            digit = num % 10
+            _sum += digit * digit
+            num //= 10
+        
+        return _sum
