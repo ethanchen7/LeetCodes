0d9c9a19b771018d2ce1b37163bb5a4c3fba6b09
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-01 15:53:41 -0700

    Time: 875 ms (25.00%), Space: 13.3 MB (79.17%) - LeetHub

--- 457-circular-array-loop/457-circular-array-loop.py
+++ 457-circular-array-loop/457-circular-array-loop.py
@@ -1,34 +1,41 @@
-class Solution:
-    def circularArrayLoop(self, nums: List[int]) -> bool:
+class Solution(object):
+    def circularArrayLoop(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
         
         for i in range(len(nums)):
-            direction = nums[i] >= 0 # boolean
             slow, fast = i, i
-        
+            direction = nums[i] > 0
+            
             while True:
-                
-                slow = self.find_next_index(nums, direction, slow)
-                fast = self.find_next_index(nums, direction, fast)
+                slow = self.new_index(nums, slow, direction)
+                fast = self.new_index(nums, fast, direction)
                 
                 if fast != -1:
-                    fast = self.find_next_index(nums, direction, fast)
+                    fast = self.new_index(nums, fast, direction)
                 
                 if slow == -1 or fast == -1 or slow == fast:
                     break
             
-            if slow != -1 and slow == fast:
+            if slow != -1 and fast != -1:
                 return True
         
         return False
+                    
+                
+        
     
-    def find_next_index(self, nums, direction, current_index):
-        current_direction = nums[current_index] >= 0
-        if direction != current_direction:
+    def new_index(self, nums, curr_index, direction):
+        
+        current_direction = nums[curr_index] > 0
+        if current_direction != direction:
             return -1
         
-        next_index = (current_index + nums[current_index]) % len(nums)
-        
-        if current_index == next_index:
+        new_index = (curr_index + nums[curr_index]) % len(nums)
+        new_direction = nums[new_index] > 0
+        if curr_index == new_index: # there is a loop
             return -1
         
-        return next_index
+        return new_index
