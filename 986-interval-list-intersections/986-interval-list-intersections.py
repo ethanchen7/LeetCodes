b84d37e3aefb28e6d583af44e51541c4aa295fbf
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-01 16:53:41 -0700

    Time: 248 ms (10.66%), Space: 14.4 MB (11.11%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -1,30 +1,31 @@
-class Solution:
-    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+class Solution(object):
+    def intervalIntersection(self, firstList, secondList):
+        """
+        :type firstList: List[List[int]]
+        :type secondList: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        # add the max start, min end of each overlapping interval
+        a, b = 0, 0
         
-        result = []
-        i, j = 0, 0
+        res = []
         
-        if not firstList or not secondList:
-            return result
-        
-        while i < len(firstList) and j < len(secondList):
+        while a < len(firstList) and b < len(secondList):
             
-            first_overlaps_second = firstList[i][0] <= secondList[j][0] and firstList[i][1] >= secondList[j][0]
+            a_overlaps_b = firstList[a][1] >= secondList[b][0] and firstList[a][0] <= secondList[b][0]
             
-            second_overlaps_first = firstList[i][0] >= secondList[j][0] and firstList[i][0] <= secondList[j][1]
-            
-            if first_overlaps_second or second_overlaps_first:
-                result.append([max(firstList[i][0], secondList[j][0]), min(firstList[i][1], secondList[j][1])])
-            
-            
-            if firstList[i][1] < secondList[j][1]:
-                i += 1
-            else:
-                j += 1
-            
-        
-        return result
+            b_overlaps_a = secondList[b][0] <= firstList[a][0] and secondList[b][1] >= firstList[a][0]
                 
-        
-        
-        
+            if a_overlaps_b or b_overlaps_a:
+                new_interval = [max(firstList[a][0], secondList[b][0]),
+                                min(firstList[a][1], secondList[b][1])]
+                
+                res.append(new_interval)
+            
+            if firstList[a][1] < secondList[b][1]:
+                a += 1
+            
+            else:
+                b += 1
+                
+        return res
