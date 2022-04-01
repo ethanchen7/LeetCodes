0307f861e8518ad002c9dba197ef9ee1dff7cf3c
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-01 16:18:53 -0700

    Time: 46 ms (99.79%), Space: 16.9 MB (19.37%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,21 +1,25 @@
-class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
         res = []
         
         for i in range(len(intervals)):
             
-            # if the end of the new interval is before the start of the first interval
             if newInterval[1] < intervals[i][0]:
                 res.append(newInterval)
                 return res + intervals[i:]
             
-            # if the interval is passed the interval we're currently checking
-            elif newInterval[0] > intervals[i][1]:
+            elif intervals[i][1] < newInterval[0]:
                 res.append(intervals[i])
             
-            else:
-                newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+            else: 
+                newInterval[0] = min(intervals[i][0], newInterval[0])
+                newInterval[1] = max(intervals[i][1], newInterval[1])
+            
         
         res.append(newInterval)
         return res
