64c5cea9250002504fd496ebfde843cc37c82724
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-02 22:53:26 -0700

    Time: 460 ms (32.36%), Space: 20.7 MB (87.06%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -4,15 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        i = 0
+        while i < len(nums):
+            j = nums[i] - 1
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
         
-        for n in nums:
-            idx = abs(n) - 1
-            nums[idx] = -1 * abs(nums[idx])
+        missing_numbers = []
+        
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                missing_numbers.append(i + 1)
+        
+        return missing_numbers
+        
+#         for n in nums:
+#             idx = abs(n) - 1
+#             nums[idx] = -1 * abs(nums[idx])
         
         
-        res = []
-        for i, n in enumerate(nums):
-            if n > 0:
-                res.append(i + 1)
+#         res = []
+#         for i, n in enumerate(nums):
+#             if n > 0:
+#                 res.append(i + 1)
         
-        return res
+#         return res
