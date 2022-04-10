f801cfc3517cafd3cbc7fcdc366ac4db34383b48
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-10 13:02:53 -0700

    Time: 110 ms (45.39%), Space: 15.5 MB (20.84%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -1,9 +1,6 @@
-class Solution(object):
-    def peakIndexInMountainArray(self, arr):
-        """
-        :type arr: List[int]
-        :rtype: int
-        """
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        maxNum = 0
         
         left = 0
         right = len(arr) - 1
@@ -12,13 +9,15 @@ class Solution(object):
             
             mid = (left + right) // 2
             
-            if arr[mid-1] < arr[mid] > arr[mid+1]:
+            # if the left side of the point is less, and the right side of the point is greater
+            # then the point must be a peak
+            
+            if arr[mid - 1] < arr[mid] > arr[mid + 1]:
                 return mid
             
-            elif arr[mid] > arr[mid + 1]:
+            # if the number to the left of mid is greater, and the number to the right less, then we need to move left more
+            elif arr[mid - 1] > arr[mid] and arr[mid] > arr[mid + 1]:
                 right = mid - 1
-            
             else:
                 left = mid + 1
-    
-                
+            
