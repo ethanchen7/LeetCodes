6c751c9c0f0f4f5cf41cfc0c8eb7518dacd78371
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-10 12:16:06 -0700

    Time: 283 ms (36.73%), Space: 14.8 MB (8.06%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -5,19 +5,21 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        left = 0
-        right = len(nums) - 1
         
-        while left <= right:
-            mid = (left + right) // 2
+        low = 0
+        high = len(nums) - 1
+        
+        while low <= high:
+            
+            mid = (low + high) // 2
             
             if nums[mid] == target:
                 return mid
             
-            if nums[mid] < target:
-                left = mid + 1
+            elif nums[mid] < target:
+                low = mid + 1
             
-            if nums[mid] > target:
-                right = mid - 1
+            else:
+                high = mid - 1
         
         return -1
