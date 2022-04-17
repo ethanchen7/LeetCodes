2e3d9fc6b18a1e3e75f979d4ba0340c063cea969
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-17 13:36:21 -0700

    Time: 20 ms (78.61%), Space: 13.5 MB (22.46%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -1,21 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        total = 0
+        return self.dfs(root, 0, total)
         
-        def dfs(node, pathSum):
-            
-            if not node: return 0
-            
-            pathSum = pathSum * 10 + node.val
-            
-            if not node.left and not node.right:
-                return pathSum
-            
-            return dfs(node.left, pathSum) + dfs(node.right, pathSum)
-    
-        return dfs(root, 0)
+        
+        
+    def dfs(self, node, currNum, total):
+        
+        if not node:
+            return 0
+        
+        currNum = currNum * 10 + node.val
+        
+        if not node.left and not node.right:
+            total += currNum
+            return total
+        
+        return self.dfs(node.left, currNum, total) + self.dfs(node.right, currNum, total)
