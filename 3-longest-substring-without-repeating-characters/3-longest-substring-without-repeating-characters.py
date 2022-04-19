e54717e7b7d23c82a7d2c523b346d9eed5506c67
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-18 18:32:09 -0700

    Time: 54 ms (75.39%), Space: 14.3 MB (30.16%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,21 +1,25 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        
-        char_frequency = {}
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         start = 0
+        char_frequency = {}
         max_len = 0
         
         for end in range(len(s)):
-            if s[end] not in char_frequency:
-                char_frequency[s[end]] = 0
-            char_frequency[s[end]] += 1
+            right = s[end]
+            if right not in char_frequency:
+                char_frequency[right] = 0
+            char_frequency[right] += 1
             
-            while char_frequency[s[end]] > 1:
+            while char_frequency[right] > 1:
                 left = s[start]
                 char_frequency[left] -= 1
                 start += 1
             
-            max_len = max(max_len, end - start + 1)
+            curr_len = end - start + 1
+            max_len = max(max_len, curr_len)
         
         return max_len
-            
