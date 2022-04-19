437242e6b4304b262d30ae966fcc33ec9229b54b
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-18 18:01:22 -0700

    Time: 14 ms (94.89%), Space: 13.4 MB (89.32%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,16 +1,16 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
         
-        window = len(needle)
+        needleLen = len(needle)
         
-        if not needle:
-            return 0
+        for i in range(0, len(haystack) - needleLen + 1):
         
-        for i in range(0, len(haystack) - window + 1):
-            if haystack[i:i+window] == needle:
+            if haystack[i:i+needleLen] == needle:
                 return i
         
         return -1
-    
-
-        
