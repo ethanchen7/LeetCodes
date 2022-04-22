ab67cdedf285d88e84e58ddc4bbabcfcceba0746
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-21 19:22:30 -0700

    Time: 56 ms (85.48%), Space: 14 MB (38.78%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -6,13 +6,15 @@ class Solution(object):
         :rtype: bool
         """
         char_frequency = {}
-        start = 0
         matched = 0
+        start = 0
         
+        # first, add all the characters to the hashmap
         for char in s1:
-            if char not in char_frequency: 
+            if char not in char_frequency:
                 char_frequency[char] = 0
             char_frequency[char] += 1
+        
         
         for end in range(len(s2)):
             right = s2[end]
@@ -21,7 +23,7 @@ class Solution(object):
                 if char_frequency[right] == 0:
                     matched += 1
             
-            if (end - start + 1 > len(s1)):
+            if (end - start + 1) > len(s1):
                 left = s2[start]
                 if left in char_frequency:
                     if char_frequency[left] == 0:
@@ -33,3 +35,4 @@ class Solution(object):
                 return True
         
         return False
+                
