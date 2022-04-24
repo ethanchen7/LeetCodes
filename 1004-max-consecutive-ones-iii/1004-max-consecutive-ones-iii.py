252416fac2788c7bfb9eeeff33d9059f745b050e
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-24 15:20:56 -0700

    Time: 647 ms (81.42%), Space: 14.7 MB (22.10%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,25 +1,22 @@
-class Solution(object):
-    def longestOnes(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        max_len = 0
-        start = 0
-        ones_frequency = 0
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        
+        # one_frequency = {}
+        start, max_len = 0, 0
         
         for end in range(len(nums)):
+            right = nums[end]
+            if right == 0:
+                k -= 1
             
-            if nums[end] == 1:
-                ones_frequency += 1
-            
-            if (end - start + 1 - ones_frequency > k):
+            if k < 0:
                 left = nums[start]
-                if left == 1: 
-                    ones_frequency -= 1
+                if left == 0:
+                    k += 1
                 start += 1
-            
-            max_len = max(max_len, end - start + 1)
+                
+            curr_len = end - start + 1
+            max_len = max(max_len, curr_len)
         
         return max_len
+            
