e334adc91e3c7eaea127f709e7a79d2e98ff48d7
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-24 15:38:28 -0700

    Time: 72 ms (54.05%), Space: 15.3 MB (93.96%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    
     def minSubArrayLen(self, target, nums):
         """
         :type target: int
@@ -6,17 +7,23 @@ class Solution(object):
         :rtype: int
         """
         
-        start = 0
-        curr_sum = 0
-        min_len = 999999
+        min_len = 99999
+        start, curr_sum = 0, 0
+        
         for end in range(len(nums)):
-            curr_sum += nums[end]
+            right = nums[end]
+            
+            curr_sum += right
             
             while curr_sum >= target:
-                min_len = min(end - start + 1, min_len)
-                curr_sum -= nums[start]
+                
+                curr_len = end - start + 1
+                min_len = min(curr_len, min_len)
+                
+                left = nums[start]
+                curr_sum -= left
                 start += 1
         
-        if min_len == 999999:
+        if min_len == 99999:
             return 0
         return min_len
