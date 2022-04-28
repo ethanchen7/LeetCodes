e013976df0a42da6ad39e1429f1cecdbe05b1fbc
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-27 23:12:30 -0700

    Time: 104 ms (53.72%), Space: 17.3 MB (55.09%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,25 +1,23 @@
-class Solution(object):
-    def insert(self, intervals, newInterval):
-        """
-        :type intervals: List[List[int]]
-        :type newInterval: List[int]
-        :rtype: List[List[int]]
-        """
-        res = []
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
+        intervals.sort(key=lambda x: x[0])
+        
+        result = []
+        # [[3,5], [6,7], [8,10]]      [4,8]
+        # [[6,7], [8,10]]             [3,8]
         for i in range(len(intervals)):
             
             if newInterval[1] < intervals[i][0]:
-                res.append(newInterval)
-                return res + intervals[i:]
+                result.append(newInterval)
+                return result + intervals[i:]
             
-            elif intervals[i][1] < newInterval[0]:
-                res.append(intervals[i])
-            
-            else: 
+            elif newInterval[0] <= intervals[i][1] and newInterval[1] >= intervals[i][0]:
                 newInterval[0] = min(intervals[i][0], newInterval[0])
                 newInterval[1] = max(intervals[i][1], newInterval[1])
             
+            else:
+                result.append(intervals[i])
         
-        res.append(newInterval)
-        return res
+        result.append(newInterval)
+        return result
