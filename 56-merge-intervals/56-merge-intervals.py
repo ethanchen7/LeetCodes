1e525442e9cbed3f0a3057f9a7ee9616b1735fc5
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-27 21:08:33 -0700

    Time: 162 ms (72.54%), Space: 18.1 MB (52.72%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,17 +1,19 @@
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        intervals.sort(key=lambda x:x[0])
-        result = [intervals[0]]
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        intervals.sort(key=lambda x: x[0])
+        
+        merged = [intervals[0]]
+        
+        if len(intervals) < 2:
+            return merged
         
         for i in range(1, len(intervals)):
-            if result[-1][1] >= intervals[i][0]:
-                result[-1][0] = min(result[-1][0], intervals[i][0])
-                result[-1][1] = max(result[-1][1], intervals[i][1])
+            if merged[-1][1] >= intervals[i][0]:
+                merged[-1][1] = max(merged[-1][1], intervals[i][1])
+                merged[-1][0] = min(merged[-1][0], intervals[i][0])
             else:
-                result.append(intervals[i])
+                merged.append(intervals[i])
         
-        return result
+        return merged
+                
