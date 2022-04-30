872bd18d339d4ca04bb4f654f9e7cf5d2a6dd294
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-04-29 21:37:27 -0700

    Time: 303 ms (9.58%), Space: 14.9 MB (15.18%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -1,26 +1,19 @@
-class Solution(object):
-    def intervalIntersection(self, firstList, secondList):
-        """
-        :type firstList: List[List[int]]
-        :type secondList: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        # add the max start, min end of each overlapping interval
-        a, b = 0, 0
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         
-        res = []
+        a, b = 0, 0
+        result = []
         
         while a < len(firstList) and b < len(secondList):
             
             a_overlaps_b = firstList[a][1] >= secondList[b][0] and firstList[a][0] <= secondList[b][0]
             
-            b_overlaps_a = secondList[b][0] <= firstList[a][0] and secondList[b][1] >= firstList[a][0]
-                
+            b_overlaps_a = firstList[a][0] >= secondList[b][0] and firstList[a][0] <= secondList[b][1]
+            
             if a_overlaps_b or b_overlaps_a:
-                new_interval = [max(firstList[a][0], secondList[b][0]),
-                                min(firstList[a][1], secondList[b][1])]
-                
-                res.append(new_interval)
+                newInterval = [max(firstList[a][0], secondList[b][0]), 
+                               min(firstList[a][1], secondList[b][1])]
+                result.append(newInterval)
             
             if firstList[a][1] < secondList[b][1]:
                 a += 1
@@ -28,4 +21,4 @@ class Solution(object):
             else:
                 b += 1
                 
-        return res
+        return result
