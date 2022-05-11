4d175eb6b641ed767164ef6d2cf8d945b469b4c1
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-05-10 22:33:06 -0700

    Time: 49 ms (12.48%), Space: 13.6 MB (48.81%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,22 +1,23 @@
-class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        
-        subsets = []
-        subsets.append([])
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         
         nums.sort()
-        
+        subsets = []
+        subsets.append([])
         start, end = 0, 0
         
         for i in range(len(nums)):
             
             start = 0
             
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i > 0 and nums[i] == nums[i-1]:
                 start = end + 1
             
             end = len(subsets) - 1
-            
             for j in range(start, end + 1):
                 set1 = list(subsets[j])
                 set1.append(nums[i])
