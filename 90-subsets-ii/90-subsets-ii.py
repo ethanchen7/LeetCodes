c18b57895fb68f6f91613726bf00ce18c394ba21
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-05-13 16:03:46 -0700

    Time: 39 ms (86.78%), Space: 14.3 MB (10.08%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,26 +1,23 @@
-class Solution(object):
-    def subsetsWithDup(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         
+        res = []
+        sub = []
         nums.sort()
-        subsets = []
-        subsets.append([])
-        start, end = 0, 0
+        self.dfs(nums, res, sub, 0)
+        return res
+    
+    def dfs(self, nums, res, sub, i):
         
-        for i in range(len(nums)):
-            
-            start = 0
-            
-            if i > 0 and nums[i] == nums[i-1]:
-                start = end + 1
-            
-            end = len(subsets) - 1
-            for j in range(start, end + 1):
-                set1 = list(subsets[j])
-                set1.append(nums[i])
-                subsets.append(set1)
+        if i == len(nums):
+            res.append(sub[:])
+            return
         
-        return subsets
+        sub.append(nums[i])
+        self.dfs(nums, res, sub, i + 1)
+        sub.pop()
+        
+        while i + 1 < len(nums) and nums[i] == nums[i+1]: #skip duplicates
+            i += 1
+        
+        self.dfs(nums, res, sub, i + 1)
