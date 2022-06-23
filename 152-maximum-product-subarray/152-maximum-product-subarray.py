b752c63d5d0d1eca52240c5d0b57cfa527e483b2
Ethan Chen <60331384+ethanchen7@users.noreply.github.com>
2022-06-23 15:59:35 -0700

    Time: 173 ms (11.72%), Space: 14.4 MB (78.69%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,21 +1,18 @@
-class Solution(object):
-    def maxProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
         res = max(nums)
-        minProd = 1
-        maxProd = 1
+        maxProd, minProd = 1, 1
         
         for n in nums:
-            if n == 0:
-                minProd = 1
-                maxProd = 1
             
-            temp = n * maxProd
+            if n == 0:
+                maxProd = 1
+                minProd = 1
+                
+            temp_max = maxProd * n
             maxProd = max(n, n*maxProd, n*minProd)
-            minProd = min(n, temp, n*minProd)
+            minProd = min(n, temp_max, n*minProd)
             
             res = max(res, maxProd)
         
