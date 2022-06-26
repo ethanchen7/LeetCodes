b093ff8e9802facfe38aed150673122998ec6da9
Ethan Chen <ethan.chen7@gmail.com>
2022-06-25 17:26:19 -0700

    Time: 234 ms (87.40%), Space: 25.4 MB (74.97%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -7,9 +7,6 @@ class Solution:
         nums.reverse()
         K = k % len(nums)
         
-        if len(nums) < 2:
-            return
-        
         left = 0
         right = K - 1
         while left < right:
@@ -17,13 +14,9 @@ class Solution:
             left += 1
             right -= 1
         
-        left2 = K
-        right2 = len(nums) - 1
-        
-        while left2 < right2:
-            nums[left2], nums[right2] = nums[right2], nums[left2]
-            left2 += 1
-            right2 -= 1
-        
-        return nums
-        
+        left = K
+        right = len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
