960fae7ccb4f4e4aa00d0bc0b61b72e6de7dd324
Ethan Chen <ethan.chen7@gmail.com>
2022-07-03 18:29:13 -0700

    Time: 801 ms (86.64%), Space: 18.1 MB (41.95%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,34 +1,36 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # [-4, -1, -1, 0, 1, 2]
         nums.sort()
-        triplets = []
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]: #continue moving the target_sum forward until no dup
-                continue 
-            self.search(triplets, nums, -nums[i], i + 1)
-        return triplets
+        result = []
+        for i in range(len(nums) - 2):
+            
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            self.search(result, nums, i)
+        
+        return result
     
-    def search(self, triplets, nums, target_sum, left):
+    def search(self, result, nums, i):
+        left = i + 1
         right = len(nums) - 1
         
         while left < right:
-            current_sum = nums[left] + nums[right]
-            if current_sum == target_sum:
-                triplets.append([-target_sum, nums[left], nums[right]])
+            current_sum = nums[i] + nums[left] + nums[right]
+            if current_sum == 0:
+                result.append([nums[i], nums[left], nums[right]])
                 left += 1
                 right -= 1
-                
-                # keep moving left and right if there are duplicates
+
                 while left < right and nums[left] == nums[left - 1]:
                     left += 1
+
                 while left < right and nums[right] == nums[right + 1]:
                     right -= 1
-            
-            elif current_sum < target_sum:
+
+            elif current_sum < 0:
                 left += 1
+
             else:
                 right -= 1
