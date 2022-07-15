99e76a7d8491670b1cf1ebb4ffcb182d53244327
Ethan Chen <ethan.chen7@gmail.com>
2022-07-14 22:04:43 -0700

    Time: N/A, Memory: N/A',
      },
      questionId: '764',
      submissionId: '747494643',
      sessionId: '4151724',
      getLangDisplay: 'python3',
      submissionCode: '\u0022\u0022\u0022\u000A# Definition for a Node.\u000Aclass Node:\u000A    def __init__(self, val\u003DNone, children\u003DNone):\u000A        self.val \u003D val\u000A        self.children \u003D children\u000A\u0022\u0022\u0022\u000A\u000Aclass Solution:\u000A    def levelOrder(self, root: \u0027Node\u0027) \u002D\u003E List[List[int]]:\u000A        \u000A        if not root:\u000A            return None\u000A        \u000A        queue \u003D deque()\u000A        queue.append(root)\u000A        result \u003D []\u000A        while queue:\u000A            temp_arr \u003D []\u000A            for i in range(len(queue)):\u000A                node \u003D queue.popleft()\u000A\u000A                if node.children:\u000A                    for child in node.children:\u000A                        queue.append(child)\u000A                \u000A                temp_arr.append(node.val)\u000A            result.append(temp_arr)\u000A        \u000A        return result\u000A                ',
      editCodeUrl: '/explore/item/3897',
      checkUrl: '/submissions/detail/747494643/check/',
      runtimeDistributionFormatted: '{\u0022lang\u0022: \u0022python3\u0022, \u0022distribution\u0022: [[\u002239\u0022, 0.0754], [\u002241\u0022, 0.1132], [\u002242\u0022, 0.0377], [\u002243\u0022, 0.1132], [\u002244\u0022, 0.2641], [\u002245\u0022, 0.5281], [\u002246\u0022, 0.4527], [\u002247\u0022, 0.5281], [\u002248\u0022, 0.8299], [\u002249\u0022, 0.679], [\u002250\u0022, 0.7167], [\u002251\u0022, 0.4149], [\u002252\u0022, 1.0939], [\u002253\u0022, 1.3203], [\u002254\u0022, 1.5466], [\u002255\u0022, 1.1694], [\u002256\u0022, 1.9992], [\u002257\u0022, 1.9238], [\u002258\u0022, 1.6598], [\u002259\u0022, 1.2448], [\u002260\u0022, 1.5466], [\u002261\u0022, 1.7729], [\u002262\u0022, 2.2256], [\u002263\u0022, 1.5843], [\u002264\u0022, 1.5089], [\u002265\u0022, 1.5466], [\u002266\u0022, 1.6975], [\u002267\u0022, 1.5466], [\u002268\u0022, 1.4334], [\u002269\u0022, 1.1694], [\u002270\u0022, 1.8484], [\u002271\u0022, 1.5466], [\u002272\u0022, 1.3203], [\u002273\u0022, 1.8106], [\u002274\u0022, 0.9808], [\u002275\u0022, 1.3957], [\u002276\u0022, 1.5843], [\u002277\u0022, 1.8484], [\u002278\u0022, 1.7729], [\u002279\u0022, 1.6975], [\u002280\u0022, 1.5466], [\u002281\u0022, 1.9238], [\u002282\u0022, 1.5466], [\u002283\u0022, 1.5089], [\u002284\u0022, 1.5466], [\u002285\u0022, 1.4711], [\u002286\u0022, 1.3203], [\u002287\u0022, 1.358], [\u002288\u0022, 1.5089], [\u002289\u0022, 1.5089], [\u002290\u0022, 1.6598], [\u002291\u0022, 1.5466], [\u002292\u0022, 1.622], [\u002293\u0022, 1.622], [\u002294\u0022, 1.5089], [\u002295\u0022, 1.3957], [\u002296\u0022, 1.0562], [\u002297\u0022, 1.6975], [\u002298\u0022, 1.3203], [\u002299\u0022, 1.4334], [\u0022100\u0022, 1.1694], [\u0022101\u0022, 0.9808], [\u0022102\u0022, 1.2071], [\u0022103\u0022, 1.0939], [\u0022104\u0022, 1.0562], [\u0022105\u0022, 1.1316], [\u0022106\u0022, 1.2071], [\u0022107\u0022, 0.943], [\u0022108\u0022, 1.0562], [\u0022109\u0022, 0.5658], [\u0022110\u0022, 0.3772], [\u0022111\u0022, 0.943], [\u0022112\u0022, 0.7167], [\u0022113\u0022, 0.6035], [\u0022114\u0022, 0.5281], [\u0022115\u0022, 0.8676], [\u0022116\u0022, 0.3395], [\u0022117\u0022, 0.4527], [\u0022118\u0022, 0.4149], [\u0022119\u0022, 0.1886], [\u0022120\u0022, 0.4904]]}',
      memoryDistributionFormatted: '{\u0022lang\u0022: \u0022python3\u0022, \u0022distribution\u0022: [[\u002215800\u0022, 0.1886], [\u002215900\u0022, 5.3565], [\u002216000\u0022, 43.8702], [\u002216100\u0022, 38.6269]]}',
      langs: [
        
      ],
      runtime: 'None',
      memory: 'None',
      enableMemoryDistribution: 'True',
      nonSufficientMsg: 'Sorry. We do not have enough accepted submissions to show distribution chart. - LeetHub

---
+++ n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        
+        if not root:
+            return None
+        
+        queue = deque()
+        queue.append(root)
+        result = []
+        while queue:
+            temp_arr = []
+            for i in range(len(queue)):
+                node = queue.popleft()
+
+                if node.children:
+                    for child in node.children:
+                        queue.append(child)
+                
+                temp_arr.append(node.val)
+            result.append(temp_arr)
+        
+        return result
+                
