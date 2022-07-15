1dc2d488b59fab36bcb391a7597f9172fdc71f91
Ethan Chen <ethan.chen7@gmail.com>
2022-07-14 21:44:59 -0700

    Time: 1235 ms (22.06%), Space: 15.4 MB (37.10%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -4,26 +4,29 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         
+        if grid[0][0] != 0 or grid[rows-1][cols-1] != 0:
+            return -1
+        
+        directions = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(-1,-1),(-1,1),(1,-1)]
+        
+        queue = collections.deque()
+        queue.append((1,(0,0)))
         visited = set()
-        queue = deque()
-        
-        directions = [(0,1),(0,-1),(1,0),(-1,0),(-1,-1),(1,1),(-1,1),(1,-1)]
-        
-        if grid[0][0] == 0:
-            queue.append((1,(0,0)))
-            visited.add((0,0))
+        visited.add((0,0))
         
         while queue:
-            dist, path = queue.popleft()
-            r, c = path[0], path[1]
+            
+            dist, coord = queue.popleft()
+            r, c = coord[0], coord[1]
+            
             if (r,c) == (rows-1, cols-1):
                 return dist
             
             for x,y in directions:
-                newR, newC = r + x, c + y
-                
-                if 0<=newR<rows and 0<=newC<cols and grid[newR][newC] == 0 and (newR, newC) not in visited:
-                    queue.append((dist+1, (newR, newC)))
-                    visited.add((newR, newC))
-                    
+                newR, newC = r+x, c+y
+                if 0<=newR<rows and 0<=newC<cols and (newR,newC) not in visited and grid[newR][newC] == 0:
+    
+                    queue.append((dist+1, (newR,newC)))
+                    visited.add((newR,newC))
+            
         return -1
