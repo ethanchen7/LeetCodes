c266012797b4f06b9f7f7211c5c69566befdc3c3
Ethan Chen <ethan.chen7@gmail.com>
2022-07-19 22:30:40 -0700

    Time: 48 ms, Memory: 13.2 MB - LeetHub

---
+++ rotate-image/rotate-image.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        self.transpose(matrix)
+        self.reflect(matrix)
+        
+    def transpose(self, matrix):
+        n = len(matrix)
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+    def reflect(self, matrix):
+        n = len(matrix)
+        
+        for i in range(n):
+            left, right = 0, n - 1
+            while left < right:
+                matrix[i][left], matrix[i][right] = matrix[i][right], matrix[i][left]
+                
+                left += 1
+                right -= 1
+            
