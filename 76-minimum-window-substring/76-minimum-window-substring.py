6ae3e7c2a23c988a4188bc86f0de68e6526e574d
Ethan Chen <ethan.chen7@gmail.com>
2022-07-26 20:12:46 -0700

    Time: 92 ms (96.18%), Space: 14.7 MB (82.83%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,45 +1,40 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        matched = 0 # must be len(t_frequency)
         start = 0
-        char_frequency = {}
-        matched = 0
-        min_len = len(s) + 1
+        min_window_len = len(s)
+        min_substr = ""
         
-        if s == t:
-            return s
         
+        t_frequency = {}
         for char in t:
-            if char not in char_frequency:
-                char_frequency[char] = 0
-            char_frequency[char] += 1
-        
+            if char not in t_frequency:
+                t_frequency[char] = 0
+            t_frequency[char] += 1
+            
         for end in range(len(s)):
             right = s[end]
-            
-            if right in char_frequency:
-                char_frequency[right] -= 1
-                if char_frequency[right] >= 0:
+            if right in t_frequency:
+                t_frequency[right] -= 1
+                if t_frequency[right] == 0:
                     matched += 1
             
-            while matched == len(t):
-                substring_length = end - start + 1
-                if substring_length < min_len:
-                    min_len = substring_length
-                    substr_start = start
-                    
+            while matched == len(t_frequency):
+                # save the window_size
+                window_size = end - start + 1
+                if window_size <= min_window_len:
+                    min_window_len = window_size
+                    min_substr = s[start:end + 1]
+
+                # shrink the window
                 left = s[start]
-                start += 1
-                if left in char_frequency:
-                    if char_frequency[left] == 0:
+                if left in t_frequency:
+                    if t_frequency[left] == 0:
                         matched -= 1
-                    char_frequency[left] += 1
+                    t_frequency[left] += 1
+                    
+                
+                start += 1
         
-        if min_len == len(s) + 1:
-            return ""
-        
-        return s[substr_start:substr_start + min_len]
+        return min_substr
