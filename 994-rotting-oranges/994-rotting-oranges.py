4a59969292842936dd3a4c2a843ba815964a5c2a
Ethan Chen <ethan.chen7@gmail.com>
2022-07-28 12:23:05 -0700

    Time: 60 ms (83.08%), Space: 13.9 MB (91.19%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,40 +1,42 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
-        
+        fresh_oranges = 0
         rows = len(grid)
         cols = len(grid[0])
+        dirs = [[0,1],[0,-1],[1,0],[-1,0]]
         
-        dirs = [(1,0),(0,1),(-1,0),(0,-1)]
         
-        queue = collections.deque()
-        
-        fresh_oranges = 0
+        queue = deque()
+        visited = set()
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1:
-                    fresh_oranges += 1
                 if grid[r][c] == 2:
                     queue.append((r,c))
+                    visited.add((r,c))
                     
-        mins_elapsed = 0
+                if grid[r][c] == 1:
+                    fresh_oranges += 1
+        time = 0
+        
         while queue and fresh_oranges > 0:
             
+           
+
             for _ in range(len(queue)):
-                orange = queue.popleft()
-                r, c = orange[0], orange[1]
-                
-                for x,y in dirs:
-                    newR, newC = r+x, c+y
-                    if 0<=newR<rows and 0<=newC<cols and grid[newR][newC] == 1:
-                        
+                r,c = queue.popleft()
+
+                for x, y in dirs:
+                    newR, newC = x + r, y + c
+                    if (newR, newC) not in visited and 0<=newR<rows and 0<=newC<cols and grid[newR][newC] == 1:
+                        visited.add((newR,newC))
+                        queue.append((newR, newC))
                         fresh_oranges -= 1
-                        
-                        grid[newR][newC] = 2
-                        queue.append((newR,newC))
             
-            mins_elapsed += 1
-            
+            time += 1
+        
         if fresh_oranges == 0:
-            return mins_elapsed
+            return time
         return -1
+        
+        
