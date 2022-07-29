10c665af5810616b9062ced37118fd6407bda06c
Ethan Chen <ethan.chen7@gmail.com>
2022-07-28 23:18:37 -0700

    Time: 7529 ms (5.12%), Space: 41.8 MB (10.06%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,30 +1,33 @@
-class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        
-        # the subset can be made if any numbers add up to 1/2 of the total sum
-        
-        if sum(nums) % 2 != 0: # we can't find equal subsets with an odd half
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if sum(nums) % 2 != 0:
             return False
         
-        n = len(nums)
-        target = sum(nums) // 2
+        s = sum(nums) // 2
         
-        dp = [[False for i in range(target + 1)] for j in range(len(nums))] # initialize 2D dp array
+        dp = [[-1 for x in range(s + 1)] for i in range(len(nums))]
         
-        # populate the first column
-        for i in range(n): # base case, 0 numbers can add up to sum of 0
-            dp[i][0] = True
+        def dfs(total, i):
+            
+            if total > s or i >= len(nums):
+                return False
+            
+            if total == s:
+                return True
+            
+            if dp[i][total] != -1:
+                return dp[i][total]
         
-        # populate the first row
-        for j in range(1, target + 1): # ex: 1 can only be the sum of itself
-            dp[0][j] = nums[0] == j # can only be True if it's equal to the first number
+            result1 = dfs(total + nums[i], i + 1)
+            result2 = dfs(total, i + 1)
+            
+            dp[i][total] = result1 or result2
+            return dp[i][total]
         
-        # process the rest of the subsets
-        for i in range(1, n):
-            for j in range(1, target + 1):
-                if dp[i - 1][j]: # if we solved the row above it, then it's the same answer
-                    dp[i][j] = dp[i - 1][j]
-                elif j >= nums[i]:
-                    dp[i][j] = dp[i - 1][j - nums[i]] # check if there's a subset with the remaining sum
+        return dfs(0, 0)
         
-        return dp[n-1][target] #check the bottom - right corner of the dp
+        
