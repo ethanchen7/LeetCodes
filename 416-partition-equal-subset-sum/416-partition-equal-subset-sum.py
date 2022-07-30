7a891c5354a0a21afd4a8b3da7052a036bd83647
Ethan Chen <ethan.chen7@gmail.com>
2022-07-30 13:44:07 -0700

    Time: 3427 ms (18.06%), Space: 42 MB (9.70%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -4,30 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        if sum(nums) % 2 != 0:
+        
+        if (sum(nums) % 2) != 0:
             return False
         
-        s = sum(nums) // 2
+        target = sum(nums) // 2
         
-        dp = [[-1 for x in range(s + 1)] for i in range(len(nums))]
+        dp = [[-1 for i in range(target + 1)] for x in range(len(nums))]
         
-        def dfs(total, i):
+        def dfs(idx, total):
             
-            if total > s or i >= len(nums):
-                return False
-            
-            if total == s:
+            if total == target:
                 return True
             
-            if dp[i][total] != -1:
-                return dp[i][total]
-        
-            result1 = dfs(total + nums[i], i + 1)
-            result2 = dfs(total, i + 1)
+            if total > target or idx >= len(nums):
+                return False
             
-            dp[i][total] = result1 or result2
-            return dp[i][total]
+            if dp[idx][total] != -1:
+                return dp[idx][total]
+            
+            total1 = dfs(idx + 1, total + nums[idx])
+            total2 = dfs(idx + 1, total)
+            
+            dp[idx][total] = (total1 or total2)
+            
+            return dp[idx][total]
         
         return dfs(0, 0)
-        
-        
