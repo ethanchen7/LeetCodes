6483f49417e2bdee0b07c116df413927b999c559
Ethan Chen <ethan.chen7@gmail.com>
2022-07-30 19:33:22 -0700

    Time: 6596 ms (11.95%), Space: 221.8 MB (5.09%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -3,27 +3,31 @@ class Solution:
         
         adj_list = {i: [] for i in range(len(points))}
         
+        # building adjacency list of distance between edges for each node
         for i in range(len(points)):
             x1, y1 = points[i][0], points[i][1]
             for j in range(i+1, len(points)):
-                x2,y2 = points[j][0], points[j][1]
-                dist = abs(x1-x2) + abs(y1-y2)
+                x2, y2 = points[j][0], points[j][1]
+                dist = abs(x1 - x2) + abs(y1 - y2)
                 adj_list[i].append([dist, j])
                 adj_list[j].append([dist, i])
         
-        res = 0
+        # using a heap to sort the closest edges to always get the closest edge
         minHeap = [[0,0]]
-        visited = set()
+        
+        visited = set() # to avoid checking an edge we already checked
+        result = 0
         
         while len(visited) < len(points):
             
-            distance, point = heapq.heappop(minHeap)
-            if point in visited:
-                continue
+            distance, node = heappop(minHeap)
             
-            res += distance
-            visited.add(point)
-            for pt in adj_list[point]:
-                heapq.heappush(minHeap, pt)
+            if node not in visited:
+                visited.add(node)
+                
+                for d, v in adj_list[node]:
+                    heappush(minHeap, [d, v]) 
+            
+                result += distance
         
-        return res
+        return result
