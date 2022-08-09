eef11f3bb48766d5442bb730ecec2cad83c07636
Ethan Chen <ethan.chen7@gmail.com>
2022-08-08 17:08:38 -0700

    Time: 12424 ms (10.46%), Space: 14.9 MB (90.22%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,17 +1,15 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         
-        jumps = 0
-        left, right = 0, 0
+        dp = [math.inf for i in range(len(nums))]
+        dp[0] = 0
         
-        while right < len(nums) - 1:
-            
-            farthest = 0
-            for i in range(left, right + 1):
-                farthest = max(farthest, i + nums[i])
-            
-            left = right + 1
-            right = farthest
-            jumps += 1
-        
-        return jumps
+        for start in range(len(nums) - 1):
+            end = start + 1
+            max_idx = start + nums[start]
+            # get to every reachable target index and check how much it costs to get there
+            while end <= max_idx and end < len(nums): 
+                dp[end] = min(dp[end], dp[start] + 1) # the cost of getting to that starting index + 1 extra step
+                end += 1
+
+        return dp[len(nums) - 1]
