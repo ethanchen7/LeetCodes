0b15ee9260a575bad5e7ba79773584dd4e69f154
Ethan Chen <ethan.chen7@gmail.com>
2022-08-09 11:23:18 -0700

    Time: 3180 ms (42.89%), Space: 31.3 MB (61.24%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -3,23 +3,16 @@ class Solution:
         
         n = len(s)
         
-        dp = [[-1 for i in range(n)] for j in range(n)]
+        dp = [[0 for i in range(n)] for j in range(n)]
         
-        def dfs(startIndex, endIndex):
-            
-            if endIndex < startIndex:
-                return 0
-            
-            if startIndex == endIndex:
-                return 1
-            
-            if dp[startIndex][endIndex] == -1:
+        for i in range(n):
+            dp[i][i] = 1
+        
+        for startIndex in range(n - 1, -1, -1):
+            for endIndex in range(startIndex + 1, n):
                 if s[startIndex] == s[endIndex]:
-                    dp[startIndex][endIndex] = 2 + dfs(startIndex + 1, endIndex - 1)
-                    
+                    dp[startIndex][endIndex] = 2 + dp[startIndex + 1][endIndex - 1]
                 else:
-                    dp[startIndex][endIndex] = max(dfs(startIndex + 1, endIndex), dfs(startIndex, endIndex - 1))
-            
-            return dp[startIndex][endIndex]
+                    dp[startIndex][endIndex] = max(dp[startIndex + 1][endIndex], dp[startIndex][endIndex - 1])
         
-        return dfs(0, n - 1)
+        return dp[0][n-1] # start to end (0 to n - 1)
