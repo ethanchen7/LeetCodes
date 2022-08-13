44536bbdaa847d0dffb67752fd34cc513b289117
Ethan Chen <ethan.chen7@gmail.com>
2022-08-13 14:32:52 -0700

    Time: 803 ms (51.68%), Space: 24.8 MB (75.54%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.py
+++ 310-minimum-height-trees/310-minimum-height-trees.py
@@ -1,38 +1,38 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         
-        if n <= 0:
-            return None
+        # remove all the leaf nodes and prune their edges
+        # keep repeating that until you are left with 1 or 2 nodes 
+        # the answer to the hint in LC is that you can only have a max of 1-2 MHTs
         
-        if n == 1:
+        if n <= 1:
             return [0]
         
         graph = {i: [] for i in range(n)}
         inDegrees = {i: 0 for i in range(n)}
         
-        for edge in edges:
-            edge1, edge2 = edge[0], edge[1]
-            graph[edge1].append(edge2)
-            graph[edge2].append(edge1)
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
             
-            inDegrees[edge1] += 1
-            inDegrees[edge2] += 1
+            inDegrees[x] += 1
+            inDegrees[y] += 1
         
-        leaves = collections.deque()
-        for vertex in inDegrees:
-            if inDegrees[vertex] == 1:
-                leaves.append(vertex)
+        leaves = deque()
+        for node in inDegrees:
+            if inDegrees[node] == 1:
+                leaves.append(node)
         
-        number_of_nodes = n
-        while number_of_nodes > 2:
-            number_of_leaves = len(leaves)
-            number_of_nodes -= number_of_leaves
-            for i in range(len(leaves)):
+        totalNodes = n
+        while totalNodes > 2:
+            leaf_count = len(leaves)
+            totalNodes -= leaf_count
+            
+            for _ in range(leaf_count):
                 leaf = leaves.popleft()
-                for child in graph[leaf]:
-                    inDegrees[child] -= 1
-                    if inDegrees[child] == 1:
-                        leaves.append(child)
+                for node in graph[leaf]:
+                    inDegrees[node] -= 1
+                    if inDegrees[node] == 1:
+                        leaves.append(node)
         
         return list(leaves)
-            
