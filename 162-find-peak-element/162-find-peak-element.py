6cbc5734e5b2b8b713eaa0a9d23758999bb56bcd
Ethan Chen <ethan.chen7@gmail.com>
2022-08-16 16:01:17 -0700

    Time: 73 ms (49.14%), Space: 13.9 MB (98.81%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -3,14 +3,27 @@ class Solution:
         
         left, right = 0, len(nums) - 1
         
-        while left < right:
+        if len(nums) == 1:
+            return 0
+        
+        while left <= right:
             
-            mid = (left + right) // 2
+            mid = left + (right - left) // 2
             
-            if nums[mid] > nums[mid + 1]:
-                right = mid
+            if mid > 0 and mid < len(nums) - 1:
+                if nums[mid - 1] < nums[mid] > nums[mid + 1]:
+                    return mid
+                
+                elif nums[mid - 1] < nums[mid] < nums[mid + 1]: # check if we're on a slope
+                    left = mid + 1  # keep going on the slope
+            
+                else:
+                    right = mid - 1
+            
+            elif mid == 0 and nums[mid] < nums[mid + 1]:
+                left = mid + 1
             
             else:
-                left = mid + 1
+                right = mid - 1
         
         return left
