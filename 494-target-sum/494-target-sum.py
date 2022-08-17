35e99d800e4573730f0ab0b98dfacb9d051c0470
Ethan Chen <ethan.chen7@gmail.com>
2022-08-17 16:39:19 -0700

    Time: 422 ms (61.76%), Space: 35.9 MB (41.92%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,9 +1,10 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        n = sum(nums)
         
-        memo = [[-math.inf for i in range(-(sum(nums) + 1), sum(nums) + 1)] for x in range(len(nums))]
+        self.memo = collections.defaultdict(int)
         
-        def dfs(idx, total):
+        def calculate(idx, total):
             
             if idx == len(nums):
                 if total == target:
@@ -11,15 +12,14 @@ class Solution:
                 else:
                     return 0
             
-            else:
-                
-                if memo[idx][total] != -math.inf:
-                    return memo[idx][total]
-                
-                sum1 = dfs(idx + 1, total + nums[idx])
-                sum2 = dfs(idx + 1, total - nums[idx])
-                
-                memo[idx][total] = sum1 + sum2
-                return memo[idx][total]
+            if (idx, total) in self.memo:
+                return self.memo[(idx, total)]
+            
+            add = calculate(idx + 1, total + nums[idx])
+            subtract = calculate(idx + 1, total - nums[idx])
+            
+            self.memo[(idx, total)] = add + subtract
+            return self.memo[(idx, total)]
         
-        return dfs(0, 0)
+        return calculate(0, 0)
+        
