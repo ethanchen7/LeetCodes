1b6f36f04274a811bbfcf283d35bdfeb351d0323
Ethan Chen <ethan.chen7@gmail.com>
2022-08-17 16:49:48 -0700

    Time: 301 ms (89.97%), Space: 15.8 MB (35.45%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,36 +1,41 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         
-        dirs = [[0,1],[0,-1],[1,0],[-1,0]]
         rows = len(heights)
         cols = len(heights[0])
+        dirs = [[1,0],[-1,0],[0,1],[0,-1]]
         
-        pac = deque()
-        atl = deque()
+        pacQ = []
+        atlQ = []
+        
+        pacVisited = set()
+        atlVisited = set()
         
         for r in range(rows):
-            pac.append((r,0))
-            atl.append((r, cols - 1))
+            pacQ.append((r,0))
+            atlQ.append((r, cols - 1))
         
         for c in range(cols):
-            pac.append((0, c))
-            atl.append((rows - 1, c))
-        
-        pac_visited = set()
-        atl_visited = set()
-        
-        def bfs(queue, visited):
+            pacQ.append((0, c))
+            atlQ.append((rows - 1, c))
+                
+        def dfs(r, c, visited):
             
-            while queue:
-                
-                r, c = queue.popleft()
-                visited.add((r,c))
-                
-                for x, y in dirs:
-                    newR, newC = r + x, c + y
-                    if 0<=newR<rows and 0<=newC<cols and heights[newR][newC] >= heights[r][c] and (newR, newC) not in visited:
-                        queue.append((newR, newC))
+            if r < 0 or r >= rows or c < 0 or c >= cols or (r,c) in visited:
+                return
+            
+            visited.add((r,c))
+            for x, y in dirs:
+                newR, newC = r + x, c + y
+                if 0<=newR<rows and 0<=newC<cols and heights[newR][newC] >= heights[r][c] and (newR, newC) not in visited:
+                    dfs(newR, newC, visited)
         
-        bfs(pac, pac_visited)
-        bfs(atl, atl_visited)
-        return pac_visited.intersection(atl_visited)
+        for r, c in pacQ:
+            if (r,c) not in pacVisited:
+                dfs(r,c, pacVisited)
+        
+        for r, c in atlQ:
+            if (r,c) not in atlVisited:
+                dfs(r,c, atlVisited)
+        
+        return pacVisited.intersection(atlVisited)
