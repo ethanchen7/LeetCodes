1dc3cacb278130ab89118780a35915af8789fbfb
Ethan Chen <ethan.chen7@gmail.com>
2022-08-17 22:21:14 -0700

    Time: 136 ms (68.99%), Space: 15.8 MB (42.83%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,34 +1,37 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         
-        parents = [i for i in range(n)]
-        ranks = [1 for i in range(n)]
-        
-        def find(x):
-            if parents[x] != x:
-                parents[x] = find(parents[x])
-            return parents[x]
-        
-        def union(x, y):
-            root_x = find(x)
-            root_y = find(y)
-            
-            if root_x == root_y:
-                return False
-            
-            if ranks[root_x] >= ranks[root_y]:
-                parents[root_y] = root_x
-                ranks[root_x] += ranks[root_y]
-            
-            else:
-                parents[root_x] = root_y
-                ranks[root_y] += ranks[root_x]
-            return True
+        graph = {i: set() for i in range(n)}
         
         for x, y in edges:
-            union(x, y)
-
-        for i in range(len(parents)):
-            find(i)
+            graph[x].add(y)
+            graph[y].add(x)
+        
+        visited = set()
+        def dfs(stack):
             
-        return len(set(parents))
+            while stack:
+                
+                node = stack.pop()
+                visited.add(node)
+                
+                for neighbor in graph[node]:
+                    
+                    if neighbor in visited:
+                        continue
+                    
+                    stack.append(neighbor)
+                    graph[neighbor].remove(node) 
+                    # remove the edge going back to the parent so we don't visit it again
+        
+        components = 0
+        for i in range(n):
+            
+            if i not in visited:
+                dfs([i])
+                components += 1
+        
+        return components
+                    
+            
+            
