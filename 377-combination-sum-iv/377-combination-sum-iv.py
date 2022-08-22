9b7ce3c0eb798afd12612d6e01613eafbef4158a
Ethan Chen <ethan.chen7@gmail.com>
2022-08-22 14:37:38 -0700

    Time: 70 ms (33.58%), Space: 13.9 MB (51.72%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,16 +1,21 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-
-        #    [1, 2, 3] nums
-        # [1, 1, 2, 4, 7] # dp
-        # pattern is that dp[i] is just a sum of all the previous combinations
         
-        dp = [0] * (target + 1)
-        dp[0] = 1
-        for i in range(1, target + 1):
+        memo = {}
+        def dfs(remain):
             
+            if remain == 0:
+                return 1
+            
+            if remain in memo:
+                return memo[remain]
+            
+            count = 0
             for n in nums:
-                if i>=n:
-                    dp[i] += dp[i - n] # add all the other possible combinations up until this point
+                if n <= remain:
+                    count += dfs(remain - n)
+            
+            memo[remain] = count
+            return memo[remain]
         
-        return dp[target]
+        return dfs(target)
