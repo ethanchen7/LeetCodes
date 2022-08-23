763d11c8d34343ba2a2a4cc60b5cd1a3a4d5bae2
Ethan Chen <ethan.chen7@gmail.com>
2022-08-22 19:33:22 -0700

    Time: 57 ms (21.58%), Space: 13.9 MB (72.24%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.py
+++ 670-maximum-swap/670-maximum-swap.py
@@ -1,18 +1,19 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
         
-        list_num = list(str(num))
-        max_idx = len(list_num) - 1
+        num_list = list(str(num))
+        max_idx = len(num_list) - 1
         
-        x, y = 0, 0
-        for i in range(len(list_num)- 1, -1, -1):
+        low, high = 0, 0
+        
+        for i in range(len(num_list) - 1, -1, -1):
             
-            if list_num[i] > list_num[max_idx]:
+            if num_list[i] > num_list[max_idx]:
                 max_idx = i
             
-            elif list_num[i] < list_num[max_idx]:
-                x = i
-                y = max_idx
+            elif num_list[i] < num_list[max_idx]:
+                low = i
+                high = max_idx
         
-        list_num[x], list_num[y] = list_num[y], list_num[x]
-        return int("".join(list_num))
+        num_list[low], num_list[high] = num_list[high], num_list[low]
+        return "".join(num_list)
