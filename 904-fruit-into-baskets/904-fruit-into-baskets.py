c05ba28b5398b86f91dd32470fb9e9faf319b027
Ethan Chen <ethan.chen7@gmail.com>
2022-08-25 10:45:10 -0700

    Time: 2265 ms (5.01%), Space: 19.7 MB (94.27%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,26 +1,28 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         
-        # we can only have two of a kind of a fruit at all times
+        fruit_freq = {}
         
-        type_frequency = {}
         start = 0
-        max_len = 0
+        max_length = 0
         
         for end in range(len(fruits)):
             right = fruits[end]
-            if right not in type_frequency:
-                type_frequency[right] = 0
-            type_frequency[right] += 1
             
-            while len(type_frequency) > 2:
+            if right not in fruit_freq:
+                fruit_freq[right] = 0
+            fruit_freq[right] += 1
+            
+            while len(fruit_freq) > 2:
                 left = fruits[start]
-                type_frequency[left] -= 1
-                if type_frequency[left] == 0:
-                    del type_frequency[left]
+                
+                fruit_freq[left] -= 1
+                if fruit_freq[left] == 0:
+                    del fruit_freq[left]
+                
                 start += 1
             
-            curr_len = end - start + 1
-            max_len = max(max_len, curr_len)
+            length = end - start + 1
+            max_length = max(length, max_length)
         
-        return max_len
+        return max_length
