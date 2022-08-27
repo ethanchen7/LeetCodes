680a0c13c0648ef50922deb2aef6bb93ce99cc26
Ethan Chen <ethan.chen7@gmail.com>
2022-08-27 13:43:06 -0700

    Time: 323 ms (9.54%), Space: 14.9 MB (51.07%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -15,12 +15,7 @@ class Solution:
             a_overlap_b = a[0] <= b[0] and a[1] >= b[0]
             b_overlap_a = a[0] >= b[0] and a[0] <= b[1]
             
-            if a_overlap_b:
-                x = max(a[0],b[0])
-                y = min(a[1],b[1])
-                intersections.append([x,y])
-            
-            elif b_overlap_a:
+            if a_overlap_b or b_overlap_a:
                 x = max(a[0],b[0])
                 y = min(a[1],b[1])
                 intersections.append([x,y])
