d1f80c22c90f6041e0ddb36c2af40309e7b2e7fd
Ethan Chen <ethan.chen7@gmail.com>
2022-08-29 11:48:13 -0700

    Time: 62 ms (18.25%), Space: 14 MB (19.63%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,16 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        if len(nums) == 1:
-            return nums[0]
+        n = len(nums)
+        dp = [0] * (n + 1)
+        dp[1] = nums[0]
         
-        dp = [0] * len(nums)
-        dp[0]= nums[0]
+        for i in range(2, len(dp)):
+            dont_rob = dp[i - 1]
+            rob = dp[i - 2] + nums[i - 1]
+            
+            dp[i] = max(dont_rob, rob)
         
-        # two decisions: rob two houses before and this one, or just don't rob this one, rob the previous one
-        for i in range(1, len(nums)):
-            rob_two_houses_before = dp[i - 2] + nums[i]
-            rob_one_house_before = dp[i - 1]
-            dp[i] = max(rob_two_houses_before, rob_one_house_before)
-
-        return max(dp)
+        return dp[-1]
+        
