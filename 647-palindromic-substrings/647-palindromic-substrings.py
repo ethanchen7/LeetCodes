767748885aba720efd3b4749c90dc6074eaab99e
Ethan Chen <ethan.chen7@gmail.com>
2022-09-02 10:58:22 -0700

    Time: 493 ms (19.67%), Space: 22.1 MB (16.37%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,5 +1,6 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        
         n = len(s)
         dp = [[False] * n for _ in range(n)]
         
@@ -8,12 +9,12 @@ class Solution:
         for i in range(n):
             dp[i][i] = True
             count += 1
-            
-        for end in range(0, n):
-            for start in range(end -1, -1, -1):
+        
+        for end in range(n):
+            for start in range(end - 1, -1, -1):
                 if s[start] == s[end]:
-                    
-                    if end-start == 1 or dp[start + 1][end - 1]:
+                    if end - start == 1 or dp[start + 1][end - 1]:
                         dp[start][end] = True
                         count += 1
+        
         return count
