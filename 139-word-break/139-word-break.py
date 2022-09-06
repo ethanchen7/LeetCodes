1a378c654863ea62e926bd55cc588475addac730
Ethan Chen <ethan.chen7@gmail.com>
2022-09-06 16:38:48 -0700

    Time: 41 ms (91.46%), Space: 14.3 MB (14.64%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,22 +1,25 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        wordSet = set(wordDict)
+        word_set = set(wordDict)
+        
         memo = {}
         
         def dfs(start):
-            if start == len(s):
-                return True
             
             if start in memo:
                 return memo[start]
             
-            for end in range(start + 1, len(s) + 1):
-                if s[start:end] in wordSet and dfs(end): # check if the substring exists and pass in the end pointer to the dfs
-                    memo[start]=True
-                    return memo[start]
+            if start >= len(s):
+                return True
+            
+            for i in range(start + 1, len(s) + 1):
+                if s[start: i] in word_set:
+                    memo[start] = dfs(i)
+                    if memo[start]: return True
             
             memo[start] = False
             return memo[start]
         
         return dfs(0)
+            
