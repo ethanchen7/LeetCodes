85ab2ce37016282a36996320e5a2ff72959f324f
Ethan Chen <ethan.chen7@gmail.com>
2022-09-06 10:42:16 -0700

    Time: 107 ms (17.90%), Space: 14.3 MB (93.89%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -5,7 +5,6 @@ class Solution:
         
         while left <= right:
             
-            # get rid of duplicates
             while left < right and nums[left] == nums[left + 1]:
                 left += 1
             
@@ -17,18 +16,20 @@ class Solution:
             if nums[mid] == target:
                 return True
             
-            if nums[mid] < nums[right]: # right side is sorted, check that our target is in there
+            if nums[mid] < nums[right]:
                 
-                if target >= nums[mid] and target <= nums[right]:
+                if target <= nums[right] and target >= nums[mid]:
                     left = mid + 1
                 
                 else:
                     right = mid - 1
             
-            else: # left side is sorted, check that our target is in there
-                if target <= nums[mid] and target >= nums[left]:
+            else:
+                
+                if target >= nums[left] and target <= nums[mid]:
                     right = mid - 1
+                
                 else:
-                    left = mid + 1
+                    left = mid + 1    
         
         return False
