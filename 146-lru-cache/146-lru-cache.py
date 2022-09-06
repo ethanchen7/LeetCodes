63c9d45f1bf04cd43fc5f0019fbf67d9c4597ec3
Ethan Chen <ethan.chen7@gmail.com>
2022-09-06 16:24:13 -0700

    Time: 1899 ms (14.15%), Space: 76.6 MB (24.70%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,35 +1,36 @@
 class Node:
-    
-    def __init__(self, key=0,val=0, next=None, prev=None):
+    def __init__(self, key=0, val=0, next=None, prev=None):
         self.key = key
         self.val = val
         self.next = next
         self.prev = prev
-    
+        
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {}
         self.capacity = capacity
+        self.cache = {}
         
-        self.head, self.tail = Node(), Node() # mru, lru
+        self.head, self.tail = Node(), Node()
         self.head.next = self.tail
         self.tail.prev = self.head
-    
+        
     def add(self, node):
-        self.head_next = self.head.next
+        head_next = self.head.next
         self.head.next = node
-        node.next = self.head_next
-        self.head_next.prev = node
+        
+        node.next = head_next
         node.prev = self.head
         
+        head_next.prev = node
+
     def remove(self, node):
         node_next = node.next
         node_prev = node.prev
         
-        node_prev.next = node_next
         node_next.prev = node_prev
-        
+        node_prev.next = node_next
+
     def get(self, key: int) -> int:
         if key in self.cache:
             node = self.cache[key]
@@ -39,20 +40,18 @@ class LRUCache:
         return -1
 
     def put(self, key: int, value: int) -> None:
-        
         if key in self.cache:
             old_node = self.cache[key]
             self.remove(old_node)
-            
-        new_node = Node(key,value)
+        
+        new_node = Node(key, value)
         self.add(new_node)
         self.cache[key] = new_node
         
         if len(self.cache) > self.capacity:
             lru = self.tail.prev
-            del self.cache[lru.key]
             self.remove(lru)
-        
+            del self.cache[lru.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
