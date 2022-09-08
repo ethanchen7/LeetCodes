e54429296d1260138b8d26d92d75e7dd642f6cc6
Ethan Chen <ethan.chen7@gmail.com>
2022-09-08 15:40:40 -0700

    Time: 1954 ms (5.34%), Space: 36.5 MB (30.55%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -5,40 +5,48 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # merge sort
         
-        if not head or not head.next:
-            return head
+        def split(head):
+            
+            if not head or not head.next:
+                return head
+            
+            slow, fast = head, head.next
+            
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            
+            right = slow.next
+            slow.next = None
+            
+            left = split(head)
+            right = split(right)
+            return merge(left, right)
         
-        slow, fast = head, head.next
-        while fast and fast.next: # get to the middle
-            slow = slow.next
-            fast = fast.next.next
+        def merge(left, right):
+            
+            dummy = ListNode()
+            tail = dummy
+            
+            while left and right:
+                
+                if left.val < right.val:
+                    dummy.next = left
+                    left = left.next
+                
+                else:
+                    dummy.next = right
+                    right = right.next
+                    
+                dummy = dummy.next
+                
+            if left:
+                dummy.next = left
+            
+            if right:
+                dummy.next = right
+            
+            return tail.next
         
-        right = slow.next # start the other half of the linked list
-        
-        slow.next = None 
-        
-        left = self.sortList(head)
-        right = self.sortList(right)
-        return self.merge(left, right)
-    
-    def merge(self, list1, list2):
-        dummy = ListNode()
-        tail = dummy
-        
-        while list1 and list2:
-            if list1.val < list2.val:
-                dummy.next = list1
-                list1 = list1.next
-            else:
-                dummy.next = list2
-                list2 = list2.next
-            dummy = dummy.next
-        
-        if list1:
-            dummy.next = list1
-        if list2:
-            dummy.next = list2
-        
-        return tail.next
+        return split(head)
