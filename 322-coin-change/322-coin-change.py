da9af14f11d6efff5ca0d68b283c2bbfbb1dc426
Ethan Chen <ethan.chen7@gmail.com>
2022-09-08 11:55:32 -0700

    Time: 1503 ms (88.34%), Space: 14.1 MB (86.25%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,22 +1,12 @@
-import math
-class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
         
-        dp = [99999 for i in range(amount + 1)]
+        dp = [math.inf for i in range(amount + 1)]
         dp[0] = 0
-        # [0, 1, 1, 2, 3, 1, 2, 2, 3, 3, 2, 3] # coin counts
-        #  0  1  2  3  4  5  6  7  8  9  10  11   amounts
-        for i in range(1,amount + 1):
+        
+        for amt in range(amount + 1):
             for c in coins:
-                if i >= c:
-                    dp[i] = min(dp[i], dp[i - c] + 1)
-
-        if dp[-1] == 99999:
-            return -1
-        else:
-            return dp[-1]
+                if amt >= c:
+                    dp[amt] = min(dp[amt], dp[amt - c] + 1)
+        
+        return dp[amount] if dp[amount] != math.inf else -1
