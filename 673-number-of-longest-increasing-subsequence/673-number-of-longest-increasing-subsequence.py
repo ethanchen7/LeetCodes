339afb3011f425bc1401c04de1ac8ff7e73ff9ef
Ethan Chen <ethan.chen7@gmail.com>
2022-09-11 14:55:58 -0700

    Time: 1460 ms (74.58%), Space: 14.2 MB (71.78%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.py
@@ -10,12 +10,12 @@ class Solution:
         for i in range(1, n):
             for j in range(0, i):
                 if nums[j] < nums[i]:
-                    if dp[i][0] < dp[j][0] + 1:
-                        dp[i][0] = dp[j][0] + 1
-                        dp[i][1] = dp[j][1]
+                    if dp[i][0] < dp[j][0] + 1: # if we found a new LIS
+                        dp[i][0] = dp[j][0] + 1 # set the LIS to that + 1
+                        dp[i][1] = dp[j][1] # set count to the old LIS count
                     
-                    elif dp[j][0] + 1 == dp[i][0]:
-                        dp[i][1] += dp[j][1]
+                    elif dp[j][0] + 1 == dp[i][0]: # if we found the same LIS
+                        dp[i][1] += dp[j][1] # increment the count
             global_max = max(global_max, dp[i][0])
         
         result = 0
