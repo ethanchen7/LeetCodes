401fcdab3ee9bffddf77375f686259da4b5969c5
Ethan Chen <ethan.chen7@gmail.com>
2022-09-12 13:22:51 -0700

    Time: 352 ms (28.16%), Space: 28 MB (78.76%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -2,39 +2,41 @@ class Trie:
 
     def __init__(self):
         self.trie = {}
-        
 
     def insert(self, word: str) -> None:
         root = self.trie
+        
         for char in word:
             if char not in root:
                 root[char] = {}
             
             root = root[char]
         
-        root['finished'] = word
+        root['$'] = word
 
     def search(self, word: str) -> bool:
+        
         root = self.trie
+        
         for char in word:
-            if char in root:
-                root = root[char]
-                # print(root)
+            if char not in root:
+                return False
+            root = root[char]
         
-        if 'finished' not in root:
-            return False
+        return '$' in root and root['$'] == word
         
-        return root['finished'] == word
 
     def startsWith(self, prefix: str) -> bool:
+        
         root = self.trie
+        
         for char in prefix:
-            if char in root:
-                root = root[char]
-            else:
+            if char not in root:
                 return False
+            root = root[char]
         
         return True
+        
 
 
 # Your Trie object will be instantiated and called as such:
