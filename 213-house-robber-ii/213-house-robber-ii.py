c2eb5421d43883fd0735ef9e8787d4114148b972
Ethan Chen <ethan.chen7@gmail.com>
2022-09-12 15:33:33 -0700

    Time: 43 ms (71.39%), Space: 13.9 MB (70.92%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -2,19 +2,19 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         
         def houseRobberOne(arr):
+            
+            if not arr:
+                return 0
+            
             dp = [0] * len(arr)
             dp[0] = arr[0]
-
-            for i in range(1, len(arr)):
-                dp[i] = max(dp[i - 1], dp[i-2] + arr[i]) # rob one before, or rob two before plus this one
-
-            return max(dp)
+            
+            for i in range(1,len(arr)):
+                dp[i] = max(dp[i-1], dp[i-2] + arr[i])
+            
+            return dp[-1]
         
-        if len(nums) < 2:
+        if len(nums) == 1:
             return nums[0]
         
-        include_first_house = houseRobberOne(nums[0:len(nums)-1])
-        include_last_house = houseRobberOne(nums[1:])
-        
-        return max(include_first_house, include_last_house)
-        
+        return max(houseRobberOne(nums[1:]), houseRobberOne(nums[:-1]))
