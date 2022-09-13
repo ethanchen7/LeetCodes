149ccd8e3f2315873eb4cfc83aa04cf9c5ac927d
Ethan Chen <ethan.chen7@gmail.com>
2022-09-12 17:03:40 -0700

    Time: 1971 ms (60.24%), Space: 15.5 MB (74.08%) - LeetHub

---
+++ 212-word-search-ii/212-word-search-ii.py
@@ -0,0 +1,45 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
+        rows = len(board)
+        cols = len(board[0])
+        result = set()
+        
+        trie = {}
+        for word in words:
+            root = trie
+            for char in word:
+                if char not in root:
+                    root[char] = {}
+                root = root[char]
+            root['WORD_KEY'] = word
+        
+        visited = set()
+        def dfs(r,c,t):
+            
+            letter = board[r][c]
+            currNode = t[letter]
+            if 'WORD_KEY' in currNode:
+                result.add(currNode['WORD_KEY'])
+                currNode.pop('WORD_KEY')
+            
+            visited.add((r,c))
+            
+            for i, j in [(-1,0),(1,0),(0,1),(0,-1)]:
+                newR, newC = r + i, c + j
+                if newR < 0 or newC < 0 or newR >= rows or newC >= cols or (newR,newC) in visited or \
+                board[newR][newC] not in currNode:
+                    continue
+                dfs(newR, newC, currNode)
+            
+            visited.remove((r,c))
+            if not currNode:
+                t.pop(letter)
+        
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] in trie: 
+                    dfs(r, c, trie)
+        
+        return list(result)
