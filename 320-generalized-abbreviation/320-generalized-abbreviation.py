393add1207d8cbd20757c64e6207966601a06e35
Ethan Chen <ethan.chen7@gmail.com>
2022-09-25 14:16:28 -0700

    Time: 522 ms (14.33%), Space: 21.3 MB (49.48%) - LeetHub

--- 320-generalized-abbreviation/320-generalized-abbreviation.py
+++ 320-generalized-abbreviation/320-generalized-abbreviation.py
@@ -1,29 +1,29 @@
 class Solution:
     def generateAbbreviations(self, word: str) -> List[str]:
-        # recursion tree
-        
-        #      abbreviate, or don't abbreviate
-        #  index0                  1      word
-        # index1               2     1ord
-        # index2            3     2rd  
-        # index3         4  3d  2r1  2rd
         
         result = []
-        n = len(word)
-        
-        def dfs(idx, curr):
+        def dfs(idx, string):
             
-            if idx == n:
-                result.append(curr)
+            if idx == len(word):
+                result.append("".join(string))
                 return
             
-            dfs(idx + 1, curr + word[idx])
+            if not string:
+                dfs(idx + 1, string + [str(1)])
+                dfs(idx + 1, string + [str(word[idx])])
             
-            for i in range(1, n - idx + 1):
-                if not curr or not curr[-1].isnumeric():
-                    dfs(idx + i, curr + str(i))
-                else:
-                    return
+            elif string[-1].isnumeric():
+                string[-1] = str(int(string[-1]) + 1)
+                dfs(idx + 1, string)
+                string[-1] = str(int(string[-1]) - 1)
+                dfs(idx + 1, string + [str(word[idx])])
+            
+            else:
+                dfs(idx + 1, string + [str(1)])
+                dfs(idx + 1, string + [str(word[idx])])
         
-        dfs(0, "")
+        
+        dfs(0, [])
         return result
+            
+            
