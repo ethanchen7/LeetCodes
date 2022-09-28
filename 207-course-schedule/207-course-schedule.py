c7355c7efa4b6a434890df4c028be6e166776aa7
Ethan Chen <ethan.chen7@gmail.com>
2022-09-28 15:02:02 -0700

    Time: 93 ms (99.32%), Space: 15.5 MB (89.66%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,30 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        sorted_order = []
+        graph = {i: [] for i in range(numCourses)}
+        inDegrees = {i: 0 for i in range(numCourses)}
         
-        graph = {i: [] for i in range(numCourses)} 
-        inDegrees = {i:0 for i in range(numCourses)}
-        
-        for crs, dep in prerequisites:
-            graph[crs].append(dep)
-            inDegrees[dep] += 1
+        for x, y in prerequisites:
+            graph[x].append(y)
+            inDegrees[y] += 1
         
         sources = deque()
-        for key in inDegrees:
-            if inDegrees[key] == 0:
-                sources.append(key)
+        for node in inDegrees:
+            if inDegrees[node] == 0:
+                sources.append(node)
         
+        sorted_order = []
         while sources:
+            
             source = sources.popleft()
             sorted_order.append(source)
             
-            for vertex in graph[source]:
-                inDegrees[vertex] -= 1
-                if inDegrees[vertex] == 0:
-                    sources.append(vertex)
+            for n in graph[source]:
+                inDegrees[n] -= 1
+                if inDegrees[n] == 0:
+                    sources.append(n)
         
-        if len(sorted_order) != numCourses:
-            return False
-        
-        return True
+        return True if len(sorted_order) == numCourses else False
