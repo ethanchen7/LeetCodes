41077af53f97ca3c6629ee185aa1bbc726007f08
Ethan Chen <ethan.chen7@gmail.com>
2022-09-28 15:23:08 -0700

    Time: 103 ms (89.01%), Space: 15.3 MB (90.49%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -1,32 +1,36 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         
-        if len(edges) != n - 1:
+        if n - 1 != len(edges):
             return False
         
-        graph = {i: set() for i in range(n)}
+        parents = [i for i in range(n)]
+        ranks = [1 for i in range(n)]
+        
+        def find(x):
+            if parents[x] != x:
+                parents[x] = find(parents[x])
+            return parents[x]
+        
+        def union(x, y):
+            root_x = find(x)
+            root_y = find(y)
+            
+            if root_x == root_y:
+                return False
+            
+            if ranks[root_x] >= ranks[root_y]:
+                parents[root_y] = root_x
+                ranks[root_x] += ranks[root_y]
+            
+            else:
+                parents[root_x] = root_y
+                ranks[root_y] += ranks[root_x]
+            
+            return True
         
         for x, y in edges:
-            graph[x].add(y)
-            graph[y].add(x)
+            if not union(x, y):
+                return False
         
-        stack = [0]
-        
-        seen = set()
-        seen.add(0)
-        
-        while stack:
-            
-            node = stack.pop()
-            
-            for neighbor in graph[node]:
-                
-                if neighbor in seen:
-                    return False
-                
-                stack.append(neighbor)
-                seen.add(neighbor)
-                graph[neighbor].remove(node)
-            
-        return len(seen) == n
-                
+        return True
