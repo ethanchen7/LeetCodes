cb7b647664372f31004e3bfd3ae270aee89ec827
Ethan Chen <ethan.chen7@gmail.com>
2022-09-27 20:18:57 -0700

    Time: 676 ms (95.14%), Space: 30.3 MB (45.46%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,9 +1,29 @@
 class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        result = []
+        
+        def dfs(start, palin):
+            
+            if start == len(s):
+                result.append(palin[:])
+                return
+            
+            for end in range(start + 1, len(s) + 1):
+                
+                if self.isValid(s[start:end]):
+                    palin.append(s[start:end])
+                    dfs(end, palin)
+                    palin.pop()
+                    
+        dfs(0, [])
+        return result
     
     def isValid(self, string):
         
         left, right = 0, len(string) - 1
-        while left <= right:
+        
+        while left < right:
             if string[left] != string[right]:
                 return False
             
@@ -11,24 +31,3 @@ class Solution:
             right -= 1
         
         return True
-    
-    def partition(self, s: str) -> List[List[str]]:
-        
-        # palin = []
-        result = []
-        
-        def dfs(start, palin):
-
-            if start == len(s):
-                result.append(palin[:])
-                return
-            
-            for end in range(start + 1, len(s) + 1):
-                
-                if self.isValid(s[start: end]):
-                    palin.append(s[start: end])
-                    dfs(end, palin)
-                    palin.pop()
-        
-        dfs(0, [])
-        return result
