6d0167af85e36051ae62b1ccd5b4518b12de3340
Ethan Chen <ethan.chen7@gmail.com>
2022-09-28 16:44:08 -0700

    Time: 3636 ms (54.05%), Space: 146.6 MB (33.49%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -3,31 +3,29 @@ class Solution:
         
         adj_list = {i: [] for i in range(len(points))}
         
-        # building adjacency list of distance between edges for each node
         for i in range(len(points)):
-            x1, y1 = points[i][0], points[i][1]
-            for j in range(i+1, len(points)):
-                x2, y2 = points[j][0], points[j][1]
-                dist = abs(x1 - x2) + abs(y1 - y2)
-                adj_list[i].append([dist, j])
-                adj_list[j].append([dist, i])
+            for j in range(i + 1, len(points)):
+                point1, point2 = points[i], points[j]
+                distance = abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+                adj_list[i].append([distance, j])
+                adj_list[j].append([distance, i])
+                
+        visited = set()
         
-        # using a heap to sort the closest edges to always get the closest edge
-        minHeap = [[0,0]]
-        
-        visited = set() # to avoid checking an edge we already checked
+        heap = [[0,0]]
         result = 0
         
         while len(visited) < len(points):
             
-            distance, node = heappop(minHeap)
+            distance, node = heappop(heap)
             
-            if node not in visited:
-                visited.add(node)
-                
-                for d, v in adj_list[node]:
-                    heappush(minHeap, [d, v]) 
+            if node in visited:
+                continue
             
-                result += distance
-        
+            visited.add(node)
+            result += distance
+            for n in adj_list[node]:
+                heappush(heap, n)
+            
+            
         return result
