8ff8bb85949b4afef6d6e8734a39fbed492854fc
Ethan Chen <ethan.chen7@gmail.com>
2022-09-27 19:15:03 -0700

    Time: 91 ms (10.46%), Space: 14.2 MB (23.41%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,21 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        perm = []
-        result = []
         
+        result = []
         def dfs(idx):
             
-            if len(perm) == len(nums):
-                result.append(perm[:])
+            if idx == len(nums):
+                result.append(nums[:])
                 return
             
-            for i in range(0, len(nums)):
-                if nums[i] not in perm:
-                    perm.append(nums[i])
-                    dfs(idx + 1)
-                    perm.pop()
-                    
+            for j in range(idx, len(nums)):
+                nums[idx], nums[j] = nums[j], nums[idx]
+                dfs(idx + 1)
+                nums[idx], nums[j] = nums[j], nums[idx]
         
         dfs(0)
         return result
