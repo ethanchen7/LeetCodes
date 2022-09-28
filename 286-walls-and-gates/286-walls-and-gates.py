812aa7842cd370b9c9055b701fb79680091b5539
Ethan Chen <ethan.chen7@gmail.com>
2022-09-28 14:52:26 -0700

    Time: 1404 ms (7.15%), Space: 16.9 MB (82.91%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -3,33 +3,35 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        row = len(rooms)
+        rows = len(rooms)
         cols = len(rooms[0])
+        queue = deque()
         
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]
-        visited = set()
+        def bfs(r,c):
+            
+            distance = 0
+            
+            queue.append((r,c))
+            visited = set()
+            
+            while queue:
+                
+                for _ in range(len(queue)):
+                    r, c = queue.popleft()
+                    visited.add((r,c))
+                    rooms[r][c] = min(rooms[r][c], distance)
+
+                    for x, y in [[-1,0],[1,0],[0,1],[0,-1]]:
+                        newR, newC = r + x, y + c
+                        if newR < 0 or newC < 0 or newR >= rows or newC >= cols or (newR, newC) in visited or \
+                        rooms[newR][newC] == -1 or rooms[newR][newC] == 0:
+                            continue
+                        queue.append((newR, newC))
+                        visited.add((newR, newC))
+                
+                distance += 1
         
-        queue = collections.deque()
-        
-        for r in range(row):
+        for r in range(rows):
             for c in range(cols):
                 if rooms[r][c] == 0:
-                    queue.append((r,c))
-                    visited.add((r,c))
-        
-        step = 0
-        while queue:
-            
-            
-            for _ in range(len(queue)):
-                r,c = queue.popleft()
-                
-                rooms[r][c] = step
-                
-                for x,y in directions:
-                    newR, newC = x + r, y + c
-                    if 0<=newR<row and 0<=newC<cols and (newR, newC) not in visited and rooms[newR][newC] != -1:
-                        queue.append((newR,newC))
-                        visited.add((newR,newC))
-            
-            step += 1
+                    bfs(r, c)
