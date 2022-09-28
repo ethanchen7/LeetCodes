e507c44a3360f6b064d31c82710ea2660ca7f154
Ethan Chen <ethan.chen7@gmail.com>
2022-09-28 14:39:32 -0700

    Time: 104 ms (34.46%), Space: 13.9 MB (48.14%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,42 +1,38 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         
-        fresh_oranges = 0
         rows = len(grid)
         cols = len(grid[0])
-        dirs = [[0,1],[0,-1],[1,0],[-1,0]]
-        
-        
-        queue = deque()
         visited = set()
+        
+        time = 0
+        fresh_oranges = 0
+        queue = deque()
+        
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 2:
-                    queue.append((r,c))
-                    visited.add((r,c))
-                    
                 if grid[r][c] == 1:
                     fresh_oranges += 1
-        time = 0
+                
+                elif grid[r][c] == 2:
+                    queue.append((r,c))
         
         while queue and fresh_oranges > 0:
             
-           
-
             for _ in range(len(queue)):
-                r,c = queue.popleft()
+                r, c = queue.popleft()
 
-                for x, y in dirs:
-                    newR, newC = x + r, y + c
-                    if (newR, newC) not in visited and 0<=newR<rows and 0<=newC<cols and grid[newR][newC] == 1:
-                        visited.add((newR,newC))
-                        queue.append((newR, newC))
-                        fresh_oranges -= 1
+                visited.add((r,c))
+
+                for x, y in [[-1,0],[1,0],[0,1],[0,-1]]:
+                    newR, newC = r + x, y + c
+                    if newR < 0 or newC < 0 or newR >= rows or newC >= cols or (newR, newC) in visited or \
+                    grid[newR][newC] != 1:
+                        continue
+                    queue.append((newR, newC))
+                    visited.add((newR, newC))
+                    fresh_oranges -= 1
             
             time += 1
         
-        if fresh_oranges == 0:
-            return time
-        return -1
-        
-        
+        return time if not fresh_oranges else -1
