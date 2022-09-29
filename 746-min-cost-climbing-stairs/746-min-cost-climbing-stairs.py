c7cd77e32739520b731789f169d7b9d6ec15d2e2
Ethan Chen <ethan.chen7@gmail.com>
2022-09-28 21:59:45 -0700

    Time: 70 ms (83.29%), Space: 14.1 MB (42.08%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,13 +1,13 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
-        dp = [0] * (len(cost) + 1) # + 1 because we need the last floor as the final step to reach
+        dp = [0] * (len(cost) + 1)
+        dp[0], dp[1] = 0, 0
         
         for i in range(2, len(cost) + 1):
+            jumpTwo = dp[i - 2] + cost[i - 2]
+            jumpOne = dp[i - 1] + cost[i - 1]
             
-            one_step = dp[i - 1] + cost[i - 1]
-            two_step = dp[i - 2] + cost[i - 2]
-            dp[i] = min(one_step, two_step)
+            dp[i] = min(jumpTwo, jumpOne)
         
         return dp[-1]
-        
