88a4b79c6159a56c8680e2655f0da6a4e92942f8
Ethan Chen <ethan.chen7@gmail.com>
2022-09-28 17:53:25 -0700

    Time: 248 ms (58.43%), Space: 15.2 MB (53.44%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,36 +1,26 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         
-        graph = {i: [] for i in range(n)}
-        for s, d, e in flights:
-            graph[s].append([e, d])
+        # for k + 1 times
+            # go through all the flights, find the new prices to reach each destination node
         
-        distances = [float("inf") for _ in range(n)]
-        current_stops = [float("inf") for _ in range(n)]
-        distances[src], current_stops[src] = 0, 0
+        prices = [math.inf for i in range(n)]
+        prices[src] = 0
         
-        minHeap = [[0, 0, src]] # cost, k, node
-        
-        while minHeap:
+        for i in range(k + 1):
             
-            cst, curr_k, node = heappop(minHeap)
-            # print(cst, curr_k, node)
-            if node == dst:
-                return cst
+            tmpPrices = prices[:]
             
-            if curr_k == k + 1:
-                continue
-            
-            for c, n in graph[node]:
+            for s, d, c in flights:
                 
-                if cst + c < distances[n]: # we found a cheaper cost to that node
-                    distances[n] = cst + c
-                    heappush(minHeap, [cst + c, curr_k + 1, n])
-                    current_stops[n] = curr_k
+                if prices[s] == math.inf:
+                    continue
                 
-                # we didn't find a cheaper cost, but we still have more stops to check,
-                # we should still keep going in case we find an overall better cost
-                elif curr_k < current_stops[n]: 
-                    heappush(minHeap, [cst + c, curr_k + 1, n])
+                if prices[s] + c < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + c
+            print(prices, 'prices')
+            print(tmpPrices, 'tmpPrices')
+            prices = tmpPrices
+            
         
-        return -1 if distances[dst] == float("inf") else distances[dst]
+        return prices[dst] if prices[dst] != math.inf else -1
